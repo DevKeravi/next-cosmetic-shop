b0bab4dbe9aaef50875592fb28ad3f0397f351ea
Keravi <cmkrosp@gmail.com>
2021-11-02 07:22:44 +0900

    sign in 페이지 디자인 완료 구현 필요

--- next-cosmetics/src/components/Gnb.tsx
+++ next-cosmetics/src/components/Gnb.tsx
@@ -1,9 +1,20 @@
 import Link from "next/link";
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Row,
+  Col,
+  Button,
+} from "react-bootstrap";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BsFillHandbagFill } from "react-icons/bs";
 import { FaUserTie } from "react-icons/fa";
 import styled from "styled-components";
+import { useState } from "react";
+import { Modal } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
 const LinkItem = styled(Nav.Link)`
   margin-left: 2vw;
@@ -11,6 +22,7 @@ const LinkItem = styled(Nav.Link)`
 `;
 
 const Gnb = () => {
+  const [toogleModal, setToogleModal] = useState(false);
   return (
     <>
       <Navbar
@@ -59,7 +71,12 @@ const Gnb = () => {
               </Link>
             </Nav>
             <Nav>
-              <LinkItem eventKey={2} href="#memes">
+              <LinkItem
+                eventKey={2}
+                onClick={() => {
+                  setToogleModal(true);
+                }}
+              >
                 <FaUserTie style={{ marginRight: "0.5vw" }} />
                 Log In
               </LinkItem>
@@ -78,6 +95,105 @@ const Gnb = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      <Modal fullscreen show={toogleModal} onHide={() => setToogleModal(false)}>
+        <Modal.Body
+          style={{ backgroundColor: "#e5e5e5" }}
+          className="show-grid"
+        >
+          <Container>
+            <Row className="g-0" style={{ textAlign: "center" }}>
+              <Col>
+                <div
+                  style={{
+                    border: "solid 2px white",
+                    color: "white",
+                    backgroundColor: "#e8e8e8",
+                    fontSize: "1.5rem",
+                    display: "inline-block",
+                    padding: "1rem",
+                    paddingLeft: "1.2rem",
+                    paddingRight: "1.2rem",
+                    marginTop: "7vh",
+                    marginBottom: "5vh",
+                  }}
+                  onClick={() => {
+                    setToogleModal(false);
+                  }}
+                >
+                  SH
+                </div>
+              </Col>
+            </Row>
+            <Row
+              className="g-0"
+              style={{
+                backgroundColor: "white",
+                maxWidth: "500px",
+                maxHeight: "500px",
+                boxShadow: "0px 0px 30px 20px #bbbbbb",
+                margin: "auto",
+              }}
+            >
+              <Col style={{ padding: "3rem" }}>
+                <Row className="g-0" style={{ textAlign: "center" }}>
+                  <Col style={{ fontSize: "2rem", marginBottom: "1.5rem" }}>
+                    SIGN IN
+                  </Col>
+                </Row>
+                <Row className="g-0">
+                  <Col>
+                    <Form>
+                      <Form.Group className="mb-4" controlId="formGroupEmail">
+                        <Form.Label
+                          style={{ color: "grey", fontSize: "0.8rem" }}
+                        >
+                          E-MAIL
+                        </Form.Label>
+                        <Form.Control type="email" />
+                      </Form.Group>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="formGroupPassword"
+                      >
+                        <Form.Label
+                          style={{ color: "grey", fontSize: "0.8rem" }}
+                        >
+                          PASSWORD
+                        </Form.Label>
+                        <Form.Control type="password" />
+                      </Form.Group>
+                      <Col style={{ textAlign: "center", marginTop: "3rem" }}>
+                        <Button
+                          style={{
+                            width: "200px",
+                            height: "50px",
+                            backgroundColor: "orange",
+                            borderRadius: "0",
+                            boxShadow: "none",
+                            border: "1px solid orange",
+                            marginBottom: "0.6rem",
+                          }}
+                        >
+                          LOG IN
+                        </Button>
+                      </Col>
+                      <Col
+                        style={{
+                          textAlign: "center",
+                          color: "orange",
+                          fontSize: "0.8rem",
+                        }}
+                      >
+                        I don't have an account
+                      </Col>
+                    </Form>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        </Modal.Body>
+      </Modal>
     </>
   );
 };
